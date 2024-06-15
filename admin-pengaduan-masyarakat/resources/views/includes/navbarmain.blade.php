3ddb44ab4e52b7529b35ae3aba40e12b16956898
22091397020HanifIhsanAlimAkbar <144103344+22091397020HanifIhsanAlimA@users.noreply.github.com>
2024-06-15 18:27:55 +0700

    .

---
+++ admin-pengaduan-masyarakat/resources/views/includes/navbarmain.blade.php
@@ -0,0 +1,34 @@
+<!-- Main Content -->
+<div id="content">
+
+    <!-- Topbar -->
+    <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+
+        <!-- Sidebar Toggle (Topbar) -->
+        <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
+            <i class="fa fa-bars"></i>
+        </button>
+
+
+
+        <!-- Topbar Navbar -->
+        <ul class="navbar-nav ml-auto">
+
+            <div class="topbar-divider d-none d-sm-block"></div>
+
+            <!-- Nav Item - User Information -->
+            <li class="nav-item dropdown no-arrow">
+                <a class="nav-link" href="/login" role="button" 
+                    aria-haspopup="true" aria-expanded="false">
+                    <span class="btn btn-primary">Login
+                        
+                    </span>
+                </a>
+                <!-- Dropdown - User Information -->
+                
+            </li>
+
+        </ul>
+
+    </nav>
+    <!-- End of Topbar -->
---
+++ admin-pengaduan-masyarakat/resources/views/public.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.main')
+
+@section('page-contents')
+    <!-- Page Heading -->
+    <div class="d-sm-flex flex-column mb-4">
+        <h1 class="h3 mb-0 text-gray-800"></h1>
+        <div class="text mt-2 ">
+            Dilaporkan Oleh : </div>
+        <div class="text mt-2 ">
+            </div>
+    </div>
+
+    <div class="d-flex flex-column card py-5 px-4">
+        <div class="w-100">
+            <img src="}" class="w-50 mx-auto d-block" alt="">
+        </div>
+        <div class="mt-5 w-100">
+            
+        </div>
+        <a href="#" class="btn btn-warning w-100 mt-3">Kembali</a>
+    </div>
+
+    <div class="d-flex flex-column my-5">
+        <div class="h4 font-weight-bold">Tanggapan</div>
+        
+            <div class="card w-100 d-flex flex-column mb-5 py-3 px-4">
+                <div class="h5 font-weight-bold" style="color: black"></div>
+                <p class="mb-4"></p>
+                <div class="w-100" style="color: black">
+                    
+                </div>
+            </div>
+        
+    </div>
+@endsection
